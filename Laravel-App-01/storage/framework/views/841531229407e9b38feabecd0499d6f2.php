1da0304320eb1f3a979643e674e399ef2999f431
JUDE-893 <ichbinarif01@gmail.com>
2024-11-12 10:12:16 +0100

    laravel learning (rootes,middlewares,csrf)

---
+++ Laravel-App-01/storage/framework/views/841531229407e9b38feabecd0499d6f2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CSRF</title>
+  </head>
+  <body>
+    You Did passed the fire wall!!
+  </body>
+</html>
+
+
+
+<?php /**PATH C:\Users\dell-pc\Desktop\Dev\Laravel-dev\Laravel-App-01\resources\views/csrf.blade.php ENDPATH**/ ?>
---
+++ Laravel-App-01/storage/framework/views/f0ce4e27de1df3dc4a2b4be40f106f46.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    You Did passed the fire wall!!
+  </body>
+</html>
+<?php /**PATH C:\Users\dell-pc\Desktop\Dev\Laravel-dev\Laravel-App-01\resources\views/page.blade.php ENDPATH**/ ?>
