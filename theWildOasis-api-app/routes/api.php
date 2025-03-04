dd6fd253bcd20ccad5b443a48e1ea39453f748e2
JUDE-893 <ichbinarif01@gmail.com>
2025-03-04 23:45:51 +0000

    update: --dependency - install laravel Breeze - install the api scafolding

--- theWildOasis-api-app/routes/api.php
+++ theWildOasis-api-app/routes/api.php
@@ -1,19 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CabinsController;
-use App\Http\Controllers\SettingsController;
-use App\Http\Controllers\BookingsController;
-use App\Http\Controllers\CabinsImagesController;
 
-// definning routes
-Route::prefix('the-wild-oasis')->group(function() {
-  Route::resource('/cabins',CabinsController::class);
-  Route::resource('/settings',SettingsController::class);
-  Route::resource('/Bookings',BookingsController::class);
-  Route::post('/Bookings/get',[BookingsController::class, 'index']);
-  Route::post('/Bookings/get-stats-after-date',[BookingsController::class, 'create']);
-  Route::post('/Bookings/get-today-activities',[BookingsController::class, 'edit']);
-  Route::resource('/bucket/cabins',CabinsImagesController::class);
-  Route::post('/bucket/cabins/update',[CabinsImagesController::class, 'updateImage']);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
